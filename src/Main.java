e509fd53109d87d868182223782682a3165586b7
Petr <Pecam464@gmail.com>
2024-05-09 12:47:13 +0200

    Nalezení a oprava chyb v proměnných a datových typech

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,64 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.math.BigDecimal;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+
+    public static void main(String[] args) {
+        ukol1();
+        ukol2();
+        ukol3();
+        ukol4();
+        ukol5();
+        ukol6();
+        ukol7();
+        ukol8();
+        ukol9();
     }
+
+    private static void ukol1() {
+            System.out.println("Hello world!");
+        }
+
+    private static void ukol2() {
+        String uzivatel = "Karel";
+        System.out.println(uzivatel);
+    }
+
+    private static void ukol3() {
+        int pocetLekci = 10;
+        System.out.println(pocetLekci);
+    }
+
+    private static void ukol4() {
+        double cena = 10.0;
+        System.out.println(cena);
+    }
+
+    private static void ukol5() {
+        double cena = 0;
+        for (int i = 0; i < 10; i++)
+            cena += 0.1;
+        System.out.println(cena);
+    }
+
+    private static void ukol6() {
+        BigDecimal cena = BigDecimal.ZERO;
+        cena = cena.add(BigDecimal.valueOf(0.1));
+        System.out.println(cena);
+    }
+
+    private static void ukol7() {
+        int vykonMotoru = 120;
+        System.out.println("výkon Motoru je: " + vykonMotoru + " kW.");
+    }
+
+    private static void ukol8() {
+        int velikostKosile = 37;
+        System.out.println("Velikost košile je: " + velikostKosile);
+    }
+
+    private static void ukol9() {
+        System.out.println("Metoda *skoro* bez chybičky!");}
+
+
 }
